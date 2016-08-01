fa0520b13370718dc841c6e806bd735dcacf36c4
Daniel Tschinder <code@tschinder.de>
2016-08-01 21:42:50 +0200

    tranform dates and check status

--- src/sqlite/eachIssue.js
+++ src/sqlite/eachIssue.js
@@ -9,23 +9,21 @@ const ISSUE_QUERY = `
 SELECT
   mt.id,
   mt.phid,
-  mt.authorPHID,
+  mt.authorPHID as creator,
   mt.title,
   mt.status,
-  mt.originalTitle,
-  mt.description,
-  mt.dateCreated,
-  mt.dateModified
+  mt.description AS body,
+  mt.dateCreated AS created_at,
+  mt.dateModified AS modified_at
 FROM maniphest_task AS mt
 ORDER BY id
 `;
 
 const COMMENT_QUERY = `
 SELECT
-  mtc.content,
-  mtc.authorPHID,
-  mtc.dateCreated,
-  mtc.dateModified
+  mtc.content AS body,
+  mtc.authorPHID as creator,
+  mtc.dateCreated AS created_at
 FROM maniphest_transaction AS mt
 INNER JOIN maniphest_transaction_comment AS mtc ON mt.commentPHID = mtc.phid
 WHERE mt.transactionType = "core:comment" AND mt.objectPHID = ?
@@ -35,19 +33,37 @@ ORDER BY mtc.dateCreated, mtc.commentVersion
 
 module.exports = function eachIssue(callback, complete) {
   const rowCallback = (err, row) => {
-    const issue = row;
     if (err) {
       log.error(err);
       return;
     }
 
     db.all(COMMENT_QUERY, row.phid, (commentErr, rows) => {
+      if (commentErr) {
+        log.error(commentErr);
+        return;
+      }
+
+      const issue = row;
+      issue.created_at = (new Date(issue.created_at * 1000)).toISOString();
+      issue.modified_at = (new Date(issue.modified_at * 1000)).toISOString();
       delete issue.phid;
 
-      issue.comments = rows;
+      if (issue.status !== 'open') {
+        issue.closed = true;
+        // TODO closed at
+      }
+      delete issue.status;
 
-      log.debug(row);
-      callback(row);
+      const comments = rows || [];
+
+      comments.forEach(comment => {
+        comment.created_at = (new Date(comment.created_at * 1000)).toISOString();
+      });
+
+      issue.comments = comments;
+
+      callback(issue);
     });
   };
 
