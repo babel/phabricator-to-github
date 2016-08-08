1756744a06cb63c7f1dd60b2dfa4371b8056285b
Daniel Tschinder <code@tschinder.de>
2016-08-08 23:23:12 +0200

    Send new issues to github

---
+++ src/github/importIssue.js
@@ -0,0 +1,25 @@
+'use strict';
+const https = require('https');
+const config = require('../../config/config');
+const log = require('../utils/log')('github');
+
+module.exports = function importIssue(issue, comments = []) {
+  const options = {
+    host: 'api.github.com',
+    path: `/repos/${config.repository}/import/issues`,
+    method: 'POST',
+    headers: {
+      Accept: 'application/vnd.github.golden-comet-preview+json',
+      Authorization: `token ${config.apiKey}`,
+      'User-Agent': 'Phabricator2Github (github.com/babel/phabricator-to-github)',
+    },
+  };
+
+  const callback = response => {
+    let str = '';
+    response.on('data', chunk => { str += chunk; });
+    response.on('end', () => log.info(str));
+  };
+
+  https.request(options, callback).end(JSON.stringify({ issue, comments }));
+};
--- src/migrate.js
+++ src/migrate.js
@@ -1,12 +1,23 @@
 'use strict';
 const eachIssue = require('./sqlite/eachIssue');
+const importIssue = require('./github/importIssue');
 const log = require('./utils/log')('migrate');
 
 module.exports = function migrate() {
+  log.info('Starting import of x issues');
   eachIssue(
     (issue, comments) => {
       log.debug(issue);
       log.debug(comments);
-    }
+
+      delete issue.creator;
+
+      comments.forEach(comment => {
+        delete comment.creator;
+      });
+
+      importIssue(issue, comments);
+    },
+    30
   );
 };
