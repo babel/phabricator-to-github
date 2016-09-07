e63dfa747fb18ccc76112c17316d6f974fad3028
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-09-07 21:43:11 +0200

    Generate redirects also for old issues

--- src/generateRedirectRepo.js
+++ src/generateRedirectRepo.js
@@ -9,6 +9,32 @@ const log = require('./utils/log')('generate redirects');
 const resultPath = path.join(__dirname, '../redirects');
 
 module.exports = function generateRedirectRepo() {
+  for (let i = 1; i <= 3086; i++) {
+    const issuePath = path.join(resultPath, `T${i}`);
+    const itemUrl = `https://github.com/${config.repository}/issues/${i}`;
+
+    const template = `<!DOCTYPE html>
+<html lang="en-US">
+<meta charset="utf-8">
+<title>Redirecting…</title>
+<link rel="canonical" href="${itemUrl}">
+<meta http-equiv="refresh" content="0; url=${itemUrl}">
+<h1>Redirecting…</h1>
+<a href="${itemUrl}">Click here if you are not redirected.</a>
+<script>location="${itemUrl}"</script>
+</html>
+`;
+    fs.mkdirs(issuePath, err => {
+      if (err) {
+        log.error(err);
+        return;
+      }
+      fs.writeFile(path.join(issuePath, 'index.html'), template, writeErr => {
+        if (writeErr) log.error(writeErr);
+      });
+    });
+  }
+
   Object.keys(redirects).forEach(issueId => {
     const githubId = redirects[issueId];
     const issuePath = path.join(resultPath, `T${issueId}`);
@@ -50,7 +76,13 @@ module.exports = function generateRedirectRepo() {
 </html>
 `;
 
-  fs.writeFile(path.join(resultPath, 'index.html'), indexTemplate, writeErr => {
-    if (writeErr) log.error(writeErr);
+  fs.mkdirs(resultPath, err => {
+    if (err) {
+      log.error(err);
+      return;
+    }
+    fs.writeFile(path.join(resultPath, 'index.html'), indexTemplate, writeErr => {
+      if (writeErr) log.error(writeErr);
+    });
   });
 };
