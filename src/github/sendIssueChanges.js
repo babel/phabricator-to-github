8c05d73cba3abffc2ec6074b0501b9a4fe68a2d5
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-09-07 11:12:55 +0200

    Increase wait timeout

--- src/github/sendIssueChanges.js
+++ src/github/sendIssueChanges.js
@@ -3,7 +3,7 @@ const log = require('../utils/log')('send-changes');
 
 module.exports = function sendIssueChanges(worker, done) {
   worker(() => {
-    const waitTime = 2000;
+    const waitTime = 3000;
     log.debug(`Waiting ${waitTime}ms for next request (github abuse limit)`);
     setTimeout(done, waitTime);
   });
