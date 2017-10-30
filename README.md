151bc799097af79634c81e8f1e1bf234b24a5649
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-10-30 17:13:30 +0100

    README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Phabricator to Github [![Build Status](https://travis-ci.org/babel/phabricator-to-github.svg?branch=master)](https://travis-ci.org/babel/phabricator-to-github)
 
+These steps have to be done in exactly this order:
+
 * copy config/default.js to config/config.js and adjust config
 * Dump all issues and comments from github to a single file with `phaway dump`
 * Preprocess all issues and comments with `phaway filter`
--- config/default.js
+++ config/default.js
@@ -4,21 +4,21 @@ module.exports = {
   // api key with at least repo permission
   apiKey: '',
   target: {
-    // the issues from this repository will be migrated
+    // the issues will be migrated to this repo
     repository: 'danez/babel',
     // Additional labels to add to the imported issues
     additionalLabels: ['imported'],
   },
   source: {
-    // the issues will be migrated to this repo
-    repository: 'babel/babylon',
+    // the issues from this repository will be migrated
+    repository: 'danez/babel-preset-env',
     // Filter which comments to move
-    filter: (issue) => true || issue.state !== 'closed',
+    filter: (issue) => issue.state !== 'closed',
     // Should issues be closed after moving
     close: true,
     // Should issues be locked after moving: false, true
-    lock: true,
-    // Add comment to moved issues in source reposiotry: callback or null
+    lock: false,
+    // Add comment to moved issues in source repository: callback or null
     comment: (repository, issueNumber) => `This issue has been moved to ${repository}#${issueNumber}.`,
   },
   // not used right now
